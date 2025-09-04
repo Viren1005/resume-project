1053235e5892d3779478fb439ecb1ecf2759c2c7
Viren1005 <viruu3471@gmail.com>
2025-09-04 23:28:15 +0530

    Project setup for Vercel

--- job-scan-main/src/lib/api.ts
+++ job-scan-main/src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // This line dynamically sets the backend URL.
 // On Vercel (production), it will be '/api'.
 // On your local machine (development), it will be 'http://localhost:8000'.
-const baseURL = import.meta.env.PROD ? "/api" : "http://localhost:8000";
+const baseURL = import.meta.env.PROD ? "https://resume-project-9w3t-d1ka2mk5h-viru-s-projects.vercel.app/" : "http://localhost:8000";
 
 const api = axios.create({
   baseURL: baseURL,
